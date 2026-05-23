6eef0ab5b0bd721e509bf94d82cf24d6e46abda5
Gabriela Regina de Souza <122756066+GabrielaSouzaDev@users.noreply.github.com>
2026-05-23 20:53:32 -0300

    alteração nome do arquivo

---
+++ calculadora_juros-simples_visual.py
@@ -0,0 +1,120 @@
+# Autor: Gabriela Souza
+# Projeto: Calculadora Simples
+# Data: 2026-05-21
+
+import customtkinter as ctk
+
+# Configuracões da janela
+ctk.set_appearance_mode("light")  # Modos: "light", "dark", "system"
+ctk.set_default_color_theme("green")  # Temas: "blue", "green", "dark-blue"
+ctk.set_widget_scaling(0.5)  # dimensões dos elementos em relação a janela
+ctk.set_window_scaling(2.0)  # dimensões da janela
+
+app = ctk.CTk()
+app.geometry("300x400")
+app.title("Calculadora de Juros Simples")
+app.iconbitmap("assets/ico/calculadora.ico")
+app.resizable(False, False) # impedir que o usuário redimensione a janela
+
+# Lógica da Calculadora
+def jurosSimples():
+    valorInvestido = float(valorInicial_entrada.get())
+    taxaJuros = float(taxaJuros_entrada.get())
+    periodo = int(periodo_entrada.get())
+
+    jurosSimples = valorInvestido * (taxaJuros / 100) * periodo
+    valorFinal = valorInvestido + jurosSimples
+
+    resultado.configure(text=(
+        f'Valor Investido: R${valorInvestido:.2f}\n'
+        f'Juros: R${jurosSimples:.2f}\n'
+        f'Valor Final: R${valorFinal:.2f}'
+        ))
+
+
+# Configurações dos elementos da interface
+# Titulo da calculadora
+titulo1 = ctk.CTkLabel(app, 
+                           text="Calculadora",
+                           font=('Arial', 50, 'bold'),
+                           text_color="#0F0F0F",
+                           )
+titulo1.pack(pady=(40, 0))
+
+titulo2 = ctk.CTkLabel(app,
+                            text="Juros Simples",
+                            font=('Arial', 40, 'bold'),
+                            text_color="#43A27D")
+titulo2.pack(pady=(0, 30))
+
+# Elementos de entrada de dados
+# Valor Inicial
+valorInicial_titulo = ctk.CTkLabel(app, 
+                                  text="Valor Inicial", 
+                                  font=('Arial', 30),
+                                  text_color="#0F0F0F")
+valorInicial_titulo.pack(pady=20)
+
+valorInicial_entrada = ctk.CTkEntry(app,
+                                  placeholder_text="R$ 0,00",
+                                  font=('Arial', 30),
+                                  width=250,
+                                  height=50)
+valorInicial_entrada.pack(pady=20)
+
+# Taxa de Juros
+taxaJuros_titulo = ctk.CTkLabel(app,
+                               text="Taxa de Juros (%)",
+                               font=('Arial', 30),
+                               text_color="#0F0F0F")
+taxaJuros_titulo.pack(pady=20)
+
+taxaJuros_entrada = ctk.CTkEntry(app,
+                               placeholder_text="0,00",
+                               font=('Arial', 30),
+                               width=250,
+                               height=50)
+taxaJuros_entrada.pack(pady=20)
+
+# Período
+periodo_titulo = ctk.CTkLabel(app,
+                            text="Período (meses)",
+                            font=('Arial', 30),
+                            text_color="#0F0F0F")
+periodo_titulo.pack(pady=20)
+
+periodo_entrada = ctk.CTkEntry(app,
+                            placeholder_text="0 meses",
+                            font=('Arial', 30),
+                            width=250,
+                            height=50)
+periodo_entrada.pack(pady=20)
+
+# Botão de calcular
+botao = ctk.CTkButton(app,
+                        text="Calcular",
+                        command=jurosSimples,
+                        corner_radius= 10,
+                        fg_color="#43A27D",
+                        hover_color="#4B9181",
+                        font=('Arial', 30, 'bold'),
+                        width=200,
+                        height=50)
+botao.pack(pady=30)
+
+# Resultado
+resultado_titulo = ctk.CTkLabel(app,
+                               text="Resultados",
+                               font=('Arial', 30, 'bold'),
+                               text_color="#43A27D")
+resultado_titulo.pack(pady=20)
+
+resultado = ctk.CTkLabel(app,
+                        text="",
+                        font=('Arial', 30, 'bold'),
+                        text_color="#0F0F0F")
+resultado.pack(pady=20)
+
+
+
+app.mainloop()
